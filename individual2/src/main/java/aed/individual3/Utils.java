566273b54ab3efb25e53cc8e5bde79b1b6c63e83
Daselsdis <publicidad5674yt7@gmail.com>
2024-10-06 20:41:10 +0200

    individual3 before great purge of code (trash)

--- individual2/src/main/java/aed/individual3/Utils.java
+++ individual2/src/main/java/aed/individual3/Utils.java
@@ -6,28 +6,52 @@ import es.upm.aedlib.positionlist.NodePositionList;
 
 public class Utils {
     public static boolean isArithmeticSequence(Iterable<Integer> l) {
+
+        Iterator<Integer> itPurge = l.iterator();
+        while (itPurge.hasNext()) {
+            boolean isNull = null == itPurge.next();
+            if (isNull) {
+                itPurge.remove();
+            }
+        }
+
         Iterator<Integer> itSize = l.iterator();
-        boolean isTrue = true;
-        int acc = 0;
+
+        int last = 0;// TODO
         int size = 0;
         while (itSize.hasNext()) {
             size++;
-            itSize.next();
+            last = itSize.next();
         }
         if (size <= 2)
             return true;
+
+        //int acc = size % 2 == 0 ? null : last; // sets the value of the substraction to the
+
         Iterator<Integer> it = l.iterator();
-        while (it.hasNext()) {
-
-            
+        Integer substraction = null;
+        Integer numPrev = null;
+        boolean isTrue = true;
+        while (it.hasNext() && isTrue) {
+            Integer numAct = it.next();
+            if (null == numAct)
+                continue;
+            if (numPrev == null) { // primera ronda
+                numPrev = numAct;
+            } else {
+                if (substraction == null) {
+                    substraction = numAct - numPrev;
+                } else {
+                    int currentSubstraction = numAct - numPrev;
+                    isTrue = substraction == currentSubstraction ? true : false;
+                }
+                numPrev = numAct;
+            }
         }
-        // while (it.hasNext()) {
-
-        // }
-        return true;
+        return isTrue;
     }
 
     public static void main(String[] args) {
-        System.out.println(isArithmeticSequence(new NodePositionList<Integer>(new Integer[] { 1 })));
+        System.out.println(isArithmeticSequence(new NodePositionList<Integer>(new Integer[] { -7,3,13,23 })));
     }
 }
