48abbd26608484893c037fc29ede01da2e4a2bf2
Daselsdis <publicidad5674yt7@gmail.com>
2024-10-19 03:01:49 +0200

    Laboratorio3 - cache get advances, before switch of repe to cursor rather than iterator, it's more appropiate for PositionList and lends easy for further mod if repe == true

--- individual2/src/main/java/aed/cache/Cache.java
+++ individual2/src/main/java/aed/cache/Cache.java
@@ -1,5 +1,7 @@
 package aed.cache;
 
+import java.util.Iterator;
+
 //import java.util.Iterator;
 
 import es.upm.aedlib.Entry;
@@ -36,8 +38,36 @@ public class Cache<Key, Value> {
 
     // Devuelve el valor que corresponde a una clave "Key"
     public Value get(Key key) {
+        // Retrieve value from key off the storage
+        Value val = mainMemory.read(key);
+
+        // Check for repeating Keys, else check for dropping
+        boolean repe = false;
+        Iterator<Key> it = keyListLRU.iterator();
+        while (it.hasNext() && !repe)
+            repe = it.next() == key;
+
+        if (repe) {
+            keyListLRU.addFirst(keyListLRU.remove(it.));
+        } else {
+
+            // Check for dropping values off the cache
+            Key droppedKey = null;
+            if (keyListLRU.size() == maxCacheSize) // If size is max, we are due to drop the last Key.
+                droppedKey = keyListLRU.remove(keyListLRU.last());
+            keyListLRU.addFirst(key); // Now we should be able to add our new Key at the start.
+
+            // Dealing with dropped values
+            if (droppedKey != null) { // If we dropped a value (accessed by it's key)
+                CacheCell<Key, Value> droppedCell = cacheContents.remove(droppedKey);
+                if (droppedCell.getDirty())
+                    mainMemory.write(droppedKey, droppedCell.getValue());
+            }
+        }
+
+        cacheContents.put(key, new CacheCell<Key, Value>(val, false, keyListLRU.first()));
         // CAMBIA este metodo
-        return null;
+        return val;
     }
 
     // Establece un valor nuevo para la clave en la memoria cache
