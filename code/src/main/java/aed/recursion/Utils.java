1bb87c4ae2d9158be496a8fddc3607bf259326a6
Daselsdis <publicidad5674yt7@gmail.com>
2024-10-25 20:25:15 +0200

    Laboratorio4 - recursion sqrt approach 1 (babylonian method) done

--- code/src/main/java/aed/recursion/Utils.java
+++ code/src/main/java/aed/recursion/Utils.java
@@ -1,24 +1,30 @@
 package aed.recursion;
 
-
 import es.upm.aedlib.Pair;
 import es.upm.aedlib.Position;
 import es.upm.aedlib.positionlist.*;
 
-
-
 public class Utils {
 
-  public static int sqrt(int n) {
-    return 2;
-  }
+    public static int sqrt(int n) {
+        return sqrtRes(n,n/Math.E);
+    }
+    
+    //* Truncated sqrt recursive function. First attempt, Babylonian method */
+    private static int sqrtRes(int a, double xn){
+        if((int)xn*(int)xn<=a&&a<=((int)xn+1)*((int)xn+1)) // Final case
+            return (int)xn;
+        else{ // General case
+            return sqrtRes(a, (xn+a/xn)/2);
+        }
+    }
 
-  public static Iterable<Integer> primes(int n) {
-    return null;
-  }
+    public static Iterable<Integer> primes(int n) {
+        return null;
+    }
 
-  public static <E> boolean equals(PositionList<Pair<E,Integer>> p1, PositionList<Pair<E,Integer>> p2) {
-    return false;
-  }
+    public static <E> boolean equals(PositionList<Pair<E, Integer>> p1, PositionList<Pair<E, Integer>> p2) {
+        return false;
+    }
 
 }
