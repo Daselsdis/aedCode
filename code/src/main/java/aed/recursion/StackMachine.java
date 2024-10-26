cfa969b6d3caea325b5bb2bb4a2ac5f408351354
Daselsdis <publicidad5674yt7@gmail.com>
2024-10-26 12:51:04 +0200

    Laboratorio4 - recursion Stack machine, no recursiva aún

--- code/src/main/java/aed/recursion/StackMachine.java
+++ code/src/main/java/aed/recursion/StackMachine.java
@@ -2,8 +2,6 @@ package aed.recursion;
 
 import es.upm.aedlib.map.*;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-
 import es.upm.aedlib.Position;
 import es.upm.aedlib.positionlist.*;
 import es.upm.aedlib.lifo.*;
@@ -19,23 +17,25 @@ public class StackMachine {
 
     public void run(String name) {
         if (code.containsKey(name)) {
-            boolean ret = false;
+            int ret = 0;
             PositionList<Instruction> prog = code.get(name);
             Position<Instruction> cursorProg = prog.first();
-            while (cursorProg != null && !ret) {
+            while (cursorProg != null && ret != -1) {
+                for (int i = 0; i < ret; i++) {
+                   cursorProg = prog.next(cursorProg);
+                } 
                 ret = execute(cursorProg, prog);
-                cursorProg = prog.next(cursorProg);
             }
         }
     }
 
-    private boolean execute(Position<Instruction> cursorProg, PositionList<Instruction> prog) {
+    private int execute(Position<Instruction> cursorProg, PositionList<Instruction> prog) {
         switch (cursorProg.element().getInstType()) {
             case CALL:
                 this.run(cursorProg.element().getNameParm());
                 break;
             case RET:
-                return true;
+                return -1;
             case PUSH:
                 stack.push(cursorProg.element().getIntParm());
                 break;
@@ -56,19 +56,15 @@ public class StackMachine {
             case EQ:
                 Integer tempEq1 = stack.pop();
                 Integer tempEq2 = stack.pop();
-                Integer eq = tempEq1.equals(tempEq2) ? (Integer)1 : (Integer)0;
+                Integer eq = tempEq1.equals(tempEq2) ? (Integer) 1 : (Integer) 0;
                 stack.push(eq);
                 break;
             case MULT:
                 stack.push(stack.pop() * stack.pop());
                 break;
             case IF_SKIP:
-                if(!stack.pop().equals((Integer)0)){
-                    int iter = cursorProg.element().getIntParm();
-                    for (int i = 0; i < iter; i++) {
-                        cursorProg = prog.next(cursorProg);
-                    }
-                }
+                if (!stack.pop().equals((Integer) 0))
+                    return cursorProg.element().getIntParm();
                 break;
             case SUB:
                 stack.push(stack.pop() - stack.pop());
@@ -82,38 +78,31 @@ public class StackMachine {
             default:
                 break;
         }
-        return false;
+        return 1;
     }
 
     public static void main(String[] args) {
-        Map<String,PositionList<Instruction>> code = new HashTableMap<>();
-            Instruction[] main = new Instruction[] {
-              new Instruction(Instruction.InstType.PUSH,5)
-              ,new Instruction(Instruction.InstType.CALL,"factorial")
-              ,new Instruction(Instruction.InstType.RET)
-            };
-            Instruction[] factorial = new Instruction[] {
-              new Instruction(Instruction.InstType.DUP)
-              ,new Instruction(Instruction.InstType.PUSH,1)
-              ,new Instruction(Instruction.InstType.EQ)
-              ,new Instruction(Instruction.InstType.IF_SKIP,8) // 1+7
-              // ELSE PART
-              ,new Instruction(Instruction.InstType.DUP)
-              ,new Instruction(Instruction.InstType.PUSH,1)
-              ,new Instruction(Instruction.InstType.SWAP)
-              ,new Instruction(Instruction.InstType.SUB)
-              ,new Instruction(Instruction.InstType.CALL,"factorial")
-              ,new Instruction(Instruction.InstType.MULT)
-              ,new Instruction(Instruction.InstType.RET)
-              // THEN PART
-              ,new Instruction(Instruction.InstType.DROP)
-              ,new Instruction(Instruction.InstType.PUSH,1)
-              ,new Instruction(Instruction.InstType.RET)
-            };
-            code.put("main",new NodePositionList<>(main));
-            code.put("factorial",new NodePositionList<>(factorial));
-            StackMachine sm = new StackMachine(code);
-            sm.run("main");
+        Map<String, PositionList<Instruction>> code = new HashTableMap<>();
+        Instruction[] main = new Instruction[] {
+                new Instruction(Instruction.InstType.PUSH, 5), new Instruction(Instruction.InstType.CALL, "factorial"),
+                new Instruction(Instruction.InstType.RET)
+        };
+        Instruction[] factorial = new Instruction[] {
+                new Instruction(Instruction.InstType.DUP), new Instruction(Instruction.InstType.PUSH, 1),
+                new Instruction(Instruction.InstType.EQ), new Instruction(Instruction.InstType.IF_SKIP, 8) // 1+7
+                // ELSE PART
+                , new Instruction(Instruction.InstType.DUP), new Instruction(Instruction.InstType.PUSH, 1),
+                new Instruction(Instruction.InstType.SWAP), new Instruction(Instruction.InstType.SUB),
+                new Instruction(Instruction.InstType.CALL, "factorial"), new Instruction(Instruction.InstType.MULT),
+                new Instruction(Instruction.InstType.RET)
+                // THEN PART
+                , new Instruction(Instruction.InstType.DROP), new Instruction(Instruction.InstType.PUSH, 1),
+                new Instruction(Instruction.InstType.RET)
+        };
+        code.put("main", new NodePositionList<>(main));
+        code.put("factorial", new NodePositionList<>(factorial));
+        StackMachine sm = new StackMachine(code);
+        sm.run("main");
     }
 
 }
