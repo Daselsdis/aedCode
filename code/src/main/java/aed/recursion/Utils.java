ae6375a7841b7495aa0cddef6317a2fce3380953
Daselsdis <publicidad5674yt7@gmail.com>
2024-10-26 21:14:25 +0200

    Laboratorio4 - recursion done

--- code/src/main/java/aed/recursion/Utils.java
+++ code/src/main/java/aed/recursion/Utils.java
@@ -4,15 +4,15 @@ import es.upm.aedlib.Pair;
 import es.upm.aedlib.Position;
 import es.upm.aedlib.positionlist.*;
 
-
-import es.upm.aedlib.indexedlist.*;;
-
 public class Utils {
 
     public static int sqrt(int n) {
         // Math.pow(2, (1/2)*(Math.log(n)/Math.log(2))) is the optimal stating
         // aproximation for general values, though, in this small usecase, n/2 is faster
-        // to calculate and good enough (these are small testing values)
+        // to calculate and good enough (these are small testing values).
+        // According to my testing, this is even faster than binary search at least on
+        // my machine. If you wish to see binary search for this purpouse, I also saved
+        // the code.
         return sqrtRec(n, n / 2);
     }
 
@@ -32,10 +32,10 @@ public class Utils {
     }
 
     private static Iterable<Integer> primesRec(Position<Integer> cursor, PositionList<Integer> grid) {
-        if (Math.pow(cursor.element(), 2) > grid.last().element())
+        if (Math.pow(cursor.element(), 2) > grid.last().element()) // Optimization proposed in the slides.
             return grid;
-        cleanProd(cursor, grid);
-        return primesRec(grid.next(cursor), grid);
+        cleanProd(cursor, grid); // Clean all multiples of the number under the current cursor.
+        return primesRec(grid.next(cursor), grid); // Do it again for the next number left after the previous purge.
     }
 
     private static void cleanProd(Position<Integer> cursor, PositionList<Integer> grid) {
@@ -52,7 +52,7 @@ public class Utils {
     }
 
     private static Position<Integer> find(PositionList<Integer> grid, int val) {
-        // FIXME: naïve implementation, prob will swap with bin search at some point if
+        // Naïve implementation, prob will swap with bin search at some point if
         // grid can be something other than a PositionList.
         return findRec(grid.first(), grid, val);
     }
@@ -66,12 +66,16 @@ public class Utils {
     }
 
     private static Integer[] advancedCribGen(int n, Integer[] crib) {
+        // I assume the size to be bigger than 3, as 2 and 3 are two consecutive primes,
+        // and cribbing within those is pointless.
         crib[0] = 2;
         crib[1] = 3;
         return advancedCribGenRes(n, crib, 2);
     }
 
     private static Integer[] advancedCribGenRes(int n, Integer[] crib, int iter) {
+        // I Pre-Purge for pair numbers, as those will by removed either way and are
+        // half the list.
         if (crib[iter - 1] - n > -2)
             return crib;
         crib[iter] = crib[iter - 1] + 2;
@@ -90,19 +94,25 @@ public class Utils {
             PositionList<Pair<E, Integer>> p2) {
         if (p1.size() != 1) {
             if (p1.next(pos1) == null)
+                // If we've reached the end of the first list without returning early,
+                // everythign has a match
                 return true;
-            return equalsDer(pos1, p2.first(), p1, p2);
+            if (!equalsDer(pos1, p2.first(), p1, p2))
+                // We check if current left obj matches with some right obj, if not, we return early a false.
+                return false;
+            // We aren't at the end, and we havne't returned early, we advance on the left list.
+            return equalsIzq(p1.next(pos1), p1, p2); 
         } else
             return pos1.element().equals(p2.first().element());
     }
 
     private static <E> boolean equalsDer(Position<Pair<E, Integer>> pos1, Position<Pair<E, Integer>> pos2,
             PositionList<Pair<E, Integer>> p1, PositionList<Pair<E, Integer>> p2) {
-        if (p2.next(pos2) == null)
-            // return equalsIzq(p1.next(pos1), p1, p2);
-            return false;
-        if (pos1.element().equals(pos2.element()))
-            return true;
+        if (pos1.element().equals(pos2.element())) // Is left obj eq to current right obj?
+            return true; // We've found match
+        if (p2.next(pos2) == null) // Is this the last right obj?
+            return false; // We haven't found a match
+        // We haven't reached the end nor found a match yet, advance to next obj right
         return equalsDer(pos1, p2.next(pos2), p1, p2);
     }
 }
